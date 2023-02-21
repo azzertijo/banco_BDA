871563dfb3aeac1396fad1b2ccae706249f72d84
SIXIVESS <mettatonbb@gmail.com>
2023-02-21 01:59:58 -0700

    Documentación y edición del Login + modificación leve del Registro

--- src/main/java/presentacion/LoginForm.java
+++ src/main/java/presentacion/LoginForm.java
@@ -15,52 +15,54 @@ import interfaces.ICuentasDAO;
 import interfaces.IDomicilioDAO;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
+
 /**
  *
- * @author JORGE
+ * @author Jorge Sánchez y Rosalía Pérez
+ */
+/**
+ * Se mandan a llamar las interfaces que se necesitarán para el login.
  */
 public class LoginForm extends javax.swing.JFrame {
-private final IClientesDAO clientesDAO;
-private final  IDomicilioDAO domicilioDAO;
-private final ICuentasDAO cuentasDAO;
-private final IConexionBD conexion;
-private Cliente clienteLog;
+
+    private final IClientesDAO clientesDAO;
+    private final IDomicilioDAO domicilioDAO;
+    private final ICuentasDAO cuentasDAO;
+    private final IConexionBD conexion;
+    private Cliente clienteLog;
+
     /**
-     * Creates new form LoginForm
+     * Creación del constructor donde se crea un nuevo cliente, domicilio y
+     * cuentas, además de ser conectadas con la base de datos.
      */
     public LoginForm(IConexionBD conexion) {
-        this.conexion=conexion;
+        this.conexion = conexion;
         this.clientesDAO = new ClientesDAO(conexion);
-        this.domicilioDAO=new DomicilioDAO (conexion);
-        this.cuentasDAO=new CuentasDAO(conexion);
+        this.domicilioDAO = new DomicilioDAO(conexion);
+        this.cuentasDAO = new CuentasDAO(conexion);
         initComponents();
     }
 
-    private Cliente extraerLogin(){
-        Cliente cliente =  new Cliente();
+    /**
+     * Se extraen los datos de la base de datos
+     * @return Los datos del cliente guardados en la base
+     */
+    private Cliente extraerLogin() {
+        Cliente cliente = new Cliente();
         String usuario = this.txtfUsuarioLogin.getText();
         String contrasenia = this.pwrdContraLogin.getText();
         cliente.setUsuario(usuario);
         cliente.setContrasenia(contrasenia);
-        
-        System.out.println(this.pwrdContraLogin.getText() +""+contrasenia);
-        
+
+        System.out.println(this.pwrdContraLogin.getText() + "" + contrasenia);
+
         cliente = clientesDAO.consultarCuenta(cliente);
-        
+
         System.out.println(cliente);
         clienteLog = cliente;
         return cliente;
     }
-    
-    
-//    private boolean validarLogin(){
-//        
-//    }
-//    private String extraerContra(){
-//        String contrasenia = this.pwrdContraLogin.getText();
-//        return contrasenia;
-//    }
-    
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -84,14 +86,14 @@ private Cliente clienteLog;
         setTitle("Banco BDA");
         setResizable(false);
 
-        jPanel1.setBackground(new java.awt.Color(231, 253, 218));
+        jPanel1.setBackground(new java.awt.Color(187, 187, 207));
 
         txtfUsuarioLogin.setFont(new java.awt.Font("Comic Sans MS", 0, 14)); // NOI18N
 
         pwrdContraLogin.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
 
-        btnRegistrarse.setBackground(new java.awt.Color(109, 150, 137));
-        btnRegistrarse.setFont(new java.awt.Font("Comic Sans MS", 0, 14)); // NOI18N
+        btnRegistrarse.setBackground(new java.awt.Color(89, 164, 232));
+        btnRegistrarse.setFont(new java.awt.Font("Consolas", 1, 14)); // NOI18N
         btnRegistrarse.setForeground(new java.awt.Color(255, 255, 255));
         btnRegistrarse.setText("Registrarse");
         btnRegistrarse.addActionListener(new java.awt.event.ActionListener() {
@@ -100,18 +102,22 @@ private Cliente clienteLog;
             }
         });
 
-        lblUsuario.setFont(new java.awt.Font("Comic Sans MS", 1, 14)); // NOI18N
+        lblUsuario.setBackground(new java.awt.Color(255, 255, 255));
+        lblUsuario.setFont(new java.awt.Font("Verdana", 1, 14)); // NOI18N
+        lblUsuario.setForeground(new java.awt.Color(255, 255, 255));
         lblUsuario.setText("Usuario");
 
-        lblContra.setFont(new java.awt.Font("Comic Sans MS", 1, 14)); // NOI18N
+        lblContra.setBackground(new java.awt.Color(255, 255, 255));
+        lblContra.setFont(new java.awt.Font("Verdana", 1, 14)); // NOI18N
+        lblContra.setForeground(new java.awt.Color(255, 255, 255));
         lblContra.setText("Contraseña");
 
-        jLabel1.setFont(new java.awt.Font("Comic Sans MS", 0, 36)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(102, 102, 102));
+        jLabel1.setFont(new java.awt.Font("Tw Cen MT Condensed Extra Bold", 1, 48)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(97, 97, 164));
         jLabel1.setText("BANCO BDA");
 
-        btnAcceder1.setBackground(new java.awt.Color(81, 183, 145));
-        btnAcceder1.setFont(new java.awt.Font("Comic Sans MS", 0, 14)); // NOI18N
+        btnAcceder1.setBackground(new java.awt.Color(218, 81, 85));
+        btnAcceder1.setFont(new java.awt.Font("Consolas", 1, 14)); // NOI18N
         btnAcceder1.setForeground(new java.awt.Color(255, 255, 255));
         btnAcceder1.setText("Acceder");
         btnAcceder1.addActionListener(new java.awt.event.ActionListener() {
@@ -120,8 +126,8 @@ private Cliente clienteLog;
             }
         });
 
-        btnRetiroSinFolio.setBackground(new java.awt.Color(109, 150, 137));
-        btnRetiroSinFolio.setFont(new java.awt.Font("Comic Sans MS", 0, 14)); // NOI18N
+        btnRetiroSinFolio.setBackground(new java.awt.Color(65, 207, 144));
+        btnRetiroSinFolio.setFont(new java.awt.Font("Consolas", 1, 14)); // NOI18N
         btnRetiroSinFolio.setForeground(new java.awt.Color(255, 255, 255));
         btnRetiroSinFolio.setText("Retiro sin folio");
         btnRetiroSinFolio.addActionListener(new java.awt.event.ActionListener() {
@@ -137,7 +143,7 @@ private Cliente clienteLog;
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(48, 48, 48)
+                        .addGap(60, 60, 60)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(txtfUsuarioLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 276, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(lblContra)
@@ -149,19 +155,19 @@ private Cliente clienteLog;
                                     .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                     .addComponent(btnRegistrarse)))))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(116, 116, 116)
-                        .addComponent(btnRetiroSinFolio))
+                        .addGap(75, 75, 75)
+                        .addComponent(jLabel1))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(74, 74, 74)
-                        .addComponent(jLabel1)))
-                .addContainerGap(67, Short.MAX_VALUE))
+                        .addGap(105, 105, 105)
+                        .addComponent(btnRetiroSinFolio)))
+                .addContainerGap(60, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap(34, Short.MAX_VALUE)
                 .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGap(30, 30, 30)
                 .addComponent(lblUsuario)
                 .addGap(3, 3, 3)
                 .addComponent(txtfUsuarioLogin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -175,7 +181,7 @@ private Cliente clienteLog;
                     .addComponent(btnAcceder1))
                 .addGap(18, 18, 18)
                 .addComponent(btnRetiroSinFolio)
-                .addGap(100, 100, 100))
+                .addGap(55, 55, 55))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -186,29 +192,40 @@ private Cliente clienteLog;
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 349, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
-
+/**
+ *  Se crea un nuevo forms al seleccionar la opción de registro
+ * @param evt Botón para el formulario de registro
+ */
     private void btnRegistrarseActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegistrarseActionPerformed
         RegistroForm registro = new RegistroForm(conexion);
         registro.setVisible(true);
         this.setVisible(false);
     }//GEN-LAST:event_btnRegistrarseActionPerformed
 
+    /**
+     *  Botón para entrar al programa una vez puesto el usuario y contraseña correcta
+     * @param evt Accede al menú
+     */
     private void btnAcceder1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAcceder1ActionPerformed
-        if(this.extraerLogin()!=null){
-            MenuForm menu = new MenuForm(conexion,clienteLog);
+        if (this.extraerLogin() != null) {
+            MenuForm menu = new MenuForm(conexion, clienteLog);
             menu.setVisible(true);
             this.setVisible(false);
-        }else{
+        } else {
             JOptionPane.showMessageDialog(this, "Ingrese credenciales validas");
         }
     }//GEN-LAST:event_btnAcceder1ActionPerformed
 
+    /**
+     * Retiro de dinero sin necesidad de un folio
+     * @param evt 
+     */
     private void btnRetiroSinFolioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRetiroSinFolioActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_btnRetiroSinFolioActionPerformed
